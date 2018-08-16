451fc2fe99c112235d93ee75770e3cb537c2640d
Jay Heng <jie.heng@nxp.com>
2018-08-16 14:35:20 +0800

    Create initial GUI
    - Created initial tinyPySPEECH.fbp by wxFormBuilder
    - Created initial main function tinypycom_main.py
    
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

---
+++ gui/tinypyspeech_win.py
@@ -0,0 +1,220 @@
+# -*- coding: utf-8 -*-
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Jul 11 2018)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+###########################################################################
+## Class speech_win
+###########################################################################
+
+class speech_win ( wx.Frame ):
+
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 940,600 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+		self.SetBackgroundColour( wx.Colour( 64, 64, 64 ) )
+
+		win_sizer = wx.WrapSizer( wx.HORIZONTAL, wx.WRAPSIZER_DEFAULT_FLAGS )
+
+		edit_win = wx.BoxSizer( wx.VERTICAL )
+
+		dir_win = wx.BoxSizer( wx.VERTICAL )
+
+		self.m_staticText_selectAudioFile = wx.StaticText( self, wx.ID_ANY, u"Select Audio File:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText_selectAudioFile.Wrap( -1 )
+
+		self.m_staticText_selectAudioFile.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_APPWORKSPACE ) )
+
+		dir_win.Add( self.m_staticText_selectAudioFile, 0, wx.ALL, 5 )
+
+		self.m_genericDirCtrl_audioDir = wx.GenericDirCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 180,100 ), wx.DIRCTRL_SHOW_FILTERS|wx.HSCROLL, wx.EmptyString, 0 )
+
+		self.m_genericDirCtrl_audioDir.ShowHidden( False )
+		self.m_genericDirCtrl_audioDir.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
+		self.m_genericDirCtrl_audioDir.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
+
+		dir_win.Add( self.m_genericDirCtrl_audioDir, 1, wx.EXPAND |wx.ALL, 5 )
+
+
+		edit_win.Add( dir_win, 1, wx.EXPAND, 5 )
+
+		rec_win = wx.WrapSizer( wx.HORIZONTAL, wx.WRAPSIZER_DEFAULT_FLAGS )
+
+		self.m_staticText_recordAudioFile = wx.StaticText( self, wx.ID_ANY, u"Record Audio File:", wx.DefaultPosition, wx.Size( 180,-1 ), 0 )
+		self.m_staticText_recordAudioFile.Wrap( -1 )
+
+		self.m_staticText_recordAudioFile.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_APPWORKSPACE ) )
+
+		rec_win.Add( self.m_staticText_recordAudioFile, 0, wx.ALL, 5 )
+
+		self.m_staticText_fileName = wx.StaticText( self, wx.ID_ANY, u"File Name:", wx.DefaultPosition, wx.Size( 60,-1 ), 0 )
+		self.m_staticText_fileName.Wrap( -1 )
+
+		self.m_staticText_fileName.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_APPWORKSPACE ) )
+
+		rec_win.Add( self.m_staticText_fileName, 0, wx.ALL, 5 )
+
+		self.m_textCtrl_fileName = wx.TextCtrl( self, wx.ID_ANY, u"Untitled 1", wx.DefaultPosition, wx.Size( 110,-1 ), 0 )
+		rec_win.Add( self.m_textCtrl_fileName, 0, wx.ALL, 5 )
+
+		self.m_staticText_sampRate = wx.StaticText( self, wx.ID_ANY, u"Samp Rate:", wx.DefaultPosition, wx.Size( 60,-1 ), 0 )
+		self.m_staticText_sampRate.Wrap( -1 )
+
+		self.m_staticText_sampRate.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_APPWORKSPACE ) )
+
+		rec_win.Add( self.m_staticText_sampRate, 0, wx.ALL, 5 )
+
+		m_choice_sampRateChoices = [ u"44100", u"22050", u"16000", u"11025" ]
+		self.m_choice_sampRate = wx.Choice( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( 80,-1 ), m_choice_sampRateChoices, 0 )
+		self.m_choice_sampRate.SetSelection( 0 )
+		rec_win.Add( self.m_choice_sampRate, 0, wx.ALL, 5 )
+
+		self.m_staticText_hz = wx.StaticText( self, wx.ID_ANY, u"Hz", wx.DefaultPosition, wx.Size( 20,-1 ), 0 )
+		self.m_staticText_hz.Wrap( -1 )
+
+		self.m_staticText_hz.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_APPWORKSPACE ) )
+
+		rec_win.Add( self.m_staticText_hz, 0, wx.ALL, 5 )
+
+		self.m_staticText_channels = wx.StaticText( self, wx.ID_ANY, u"Channels:", wx.DefaultPosition, wx.Size( 60,-1 ), 0 )
+		self.m_staticText_channels.Wrap( -1 )
+
+		self.m_staticText_channels.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_APPWORKSPACE ) )
+
+		rec_win.Add( self.m_staticText_channels, 0, wx.ALL, 5 )
+
+		m_choice_channelsChoices = [ u"Mono", u"Stereo" ]
+		self.m_choice_channels = wx.Choice( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( 80,-1 ), m_choice_channelsChoices, 0 )
+		self.m_choice_channels.SetSelection( 1 )
+		rec_win.Add( self.m_choice_channels, 0, wx.ALL, 5 )
+
+		self.m_staticText_bitDepth = wx.StaticText( self, wx.ID_ANY, u"Bit Depth:", wx.DefaultPosition, wx.Size( 60,-1 ), 0 )
+		self.m_staticText_bitDepth.Wrap( -1 )
+
+		self.m_staticText_bitDepth.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_APPWORKSPACE ) )
+
+		rec_win.Add( self.m_staticText_bitDepth, 0, wx.ALL, 5 )
+
+		m_choice_bitDepthChoices = [ u"8", u"16", u"24", u"32" ]
+		self.m_choice_bitDepth = wx.Choice( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( 80,-1 ), m_choice_bitDepthChoices, 0 )
+		self.m_choice_bitDepth.SetSelection( 1 )
+		rec_win.Add( self.m_choice_bitDepth, 0, wx.ALL, 5 )
+
+		self.m_staticText_bits = wx.StaticText( self, wx.ID_ANY, u"bits", wx.DefaultPosition, wx.Size( 20,-1 ), 0 )
+		self.m_staticText_bits.Wrap( -1 )
+
+		self.m_staticText_bits.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_APPWORKSPACE ) )
+
+		rec_win.Add( self.m_staticText_bits, 0, wx.ALL, 5 )
+
+		self.m_staticText_null0 = wx.StaticText( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 30,-1 ), 0 )
+		self.m_staticText_null0.Wrap( -1 )
+
+		rec_win.Add( self.m_staticText_null0, 0, wx.ALL, 5 )
+
+		self.m_button_record = wx.Button( self, wx.ID_ANY, u"Record Start", wx.DefaultPosition, wx.Size( 110,-1 ), 0 )
+		rec_win.Add( self.m_button_record, 0, wx.ALL, 5 )
+
+
+		edit_win.Add( rec_win, 1, wx.EXPAND, 5 )
+
+
+		win_sizer.Add( edit_win, 1, wx.EXPAND, 5 )
+
+		show_win = wx.BoxSizer( wx.VERTICAL )
+
+		plot_win = wx.BoxSizer( wx.VERTICAL )
+
+		self.m_panel_plot = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( 720,360 ), wx.TAB_TRAVERSAL )
+		self.m_panel_plot.SetFont( wx.Font( 9, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "宋体" ) )
+		self.m_panel_plot.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
+		self.m_panel_plot.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
+
+		plot_win.Add( self.m_panel_plot, 1, wx.EXPAND |wx.ALL, 5 )
+
+
+		show_win.Add( plot_win, 1, wx.EXPAND, 5 )
+
+		hmi_win = wx.BoxSizer( wx.VERTICAL )
+
+		ctrl_win = wx.WrapSizer( wx.HORIZONTAL, wx.WRAPSIZER_DEFAULT_FLAGS )
+
+		self.m_staticText_null1 = wx.StaticText( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 130,-1 ), 0 )
+		self.m_staticText_null1.Wrap( -1 )
+
+		ctrl_win.Add( self.m_staticText_null1, 0, wx.ALL, 5 )
+
+		self.m_button_play = wx.Button( self, wx.ID_ANY, u"Play Start", wx.DefaultPosition, wx.Size( 110,-1 ), 0 )
+		ctrl_win.Add( self.m_button_play, 0, wx.ALL, 5 )
+
+		self.m_staticText_volume = wx.StaticText( self, wx.ID_ANY, u"Volume:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText_volume.Wrap( -1 )
+
+		self.m_staticText_volume.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_APPWORKSPACE ) )
+
+		ctrl_win.Add( self.m_staticText_volume, 0, wx.ALL, 5 )
+
+		self.m_slider_volume = wx.Slider( self, wx.ID_ANY, 50, 0, 100, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
+		ctrl_win.Add( self.m_slider_volume, 0, wx.ALL, 5 )
+
+		self.m_staticText_velocity = wx.StaticText( self, wx.ID_ANY, u"Velocity:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText_velocity.Wrap( -1 )
+
+		self.m_staticText_velocity.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_APPWORKSPACE ) )
+
+		ctrl_win.Add( self.m_staticText_velocity, 0, wx.ALL, 5 )
+
+		self.m_slider_velocity = wx.Slider( self, wx.ID_ANY, 50, 0, 100, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
+		ctrl_win.Add( self.m_slider_velocity, 0, wx.ALL, 5 )
+
+
+		hmi_win.Add( ctrl_win, 1, wx.EXPAND, 5 )
+
+		text_win = wx.WrapSizer( wx.HORIZONTAL, wx.WRAPSIZER_DEFAULT_FLAGS )
+
+		self.m_textCtrl2 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 720,150 ), 0|wx.HSCROLL|wx.VSCROLL )
+		text_win.Add( self.m_textCtrl2, 0, wx.ALL, 5 )
+
+
+		hmi_win.Add( text_win, 1, wx.EXPAND, 5 )
+
+
+		show_win.Add( hmi_win, 1, wx.EXPAND, 5 )
+
+
+		win_sizer.Add( show_win, 1, wx.EXPAND, 5 )
+
+
+		self.SetSizer( win_sizer )
+		self.Layout()
+
+		self.Centre( wx.BOTH )
+
+		# Connect Events
+		self.m_genericDirCtrl_audioDir.Bind( wx.EVT_TREE_SEL_CHANGED, self.viewAudio )
+		self.m_button_record.Bind( wx.EVT_BUTTON, self.recordAudio )
+		self.m_button_play.Bind( wx.EVT_BUTTON, self.playAudio )
+
+	def __del__( self ):
+		pass
+
+
+	# Virtual event handlers, overide them in your derived class
+	def viewAudio( self, event ):
+		event.Skip()
+
+	def recordAudio( self, event ):
+		event.Skip()
+
+	def playAudio( self, event ):
+		event.Skip()
+
+
---
+++ src/tinypyspeech_main.py
@@ -0,0 +1,93 @@
+import wx
+import sys, os
+sys.path.append(os.path.abspath("../gui"))
+import tinypyspeech_win
+import numpy
+import matplotlib
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+import wave
+
+MAX_AUDIO_CHANNEL = 8
+#unit: inch
+PLOT_PANEL_WIDTH = 720
+PLOT_PANEL_HEIGHT = 360
+#unit: percent
+PLOT_AXES_WIDTH_TITLE = 0.05
+PLOT_AXES_HEIGHT_LABEL = 0.075
+
+class wavCanvasPanel(wx.Panel):
+
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        dpi = 60
+        width = PLOT_PANEL_WIDTH / dpi
+        height = PLOT_PANEL_HEIGHT / dpi
+        self.wavFigure = Figure(figsize=[width,height], dpi=dpi, facecolor='#404040')
+        self.wavCanvas = FigureCanvas(self, -1, self.wavFigure)
+        self.wavSizer = wx.BoxSizer(wx.VERTICAL)
+        self.wavSizer.Add(self.wavCanvas, 1, wx.EXPAND|wx.ALL)
+        self.SetSizerAndFit(self.wavSizer)
+
+    def readWave(self, wavPath):
+        if os.path.isfile(wavPath):
+            # Open the wav file to get wave data and parameters
+            wavFile =  wave.open(wavPath, "rb")
+            wavParams = wavFile.getparams()
+            wavChannels = wavParams[0]
+            wavFramerate = wavParams[2]
+            wavFrames = wavParams[3]
+            wavData = wavFile.readframes(wavFrames)
+            wavFile.close()
+            # Transpose the wav data if wave has multiple channels
+            retData = numpy.fromstring(wavData, dtype = numpy.short)
+            retData.shape = -1, 2
+            retData = retData.T
+            # Calculate and arange wave time
+            retTime = numpy.arange(0, wavFrames) * (1.0 / wavFramerate)
+            retChannels = wavChannels
+            return retChannels, retData, retTime
+        else:
+            return 0, 0, 0
+
+    def showWave(self, wavPath):
+        self.wavFigure.clear()
+        waveChannels, waveData, waveTime = self.readWave(wavPath)
+        if waveChannels != 0:
+            # Note: only show max supported channel if actual channel > max supported channel
+            if waveChannels > MAX_AUDIO_CHANNEL:
+                waveChannels = MAX_AUDIO_CHANNEL
+            # Polt the waveform of each channel in sequence
+            for i in range(waveChannels):
+                left = PLOT_AXES_HEIGHT_LABEL
+                bottom = (1.0 / waveChannels) * (waveChannels - 1 - i) + PLOT_AXES_HEIGHT_LABEL
+                height = 1.0 / waveChannels - (PLOT_AXES_WIDTH_TITLE + PLOT_AXES_HEIGHT_LABEL)
+                width = 1 - left - 0.05
+                wavAxes = self.wavFigure.add_axes([left, bottom, width, height], facecolor='k')
+                wavAxes.set_prop_cycle(color='#00F279', lw=[1])
+                wavAxes.set_xlabel('time (s)', color='w')
+                wavAxes.set_ylabel('value', color='w')
+                wavAxes.plot(waveTime, waveData[i])
+                wavAxes.tick_params(labelcolor='w')
+                wavAxes.set_title('Audio Channel ' + str(i), color='w')
+
+class mainWin(tinypyspeech_win.speech_win):
+
+    def __init__(self, parent):
+        tinypyspeech_win.speech_win.__init__(self, parent)
+        self.wavPanel = wavCanvasPanel(self.m_panel_plot)
+        self.m_genericDirCtrl_audioDir.SetFilter("Audio files (*.wav)|*.wav")
+
+    def viewAudio( self, event ):
+        self.wavPath =  self.m_genericDirCtrl_audioDir.GetFilePath()
+        self.wavPanel.showWave(self.wavPath)
+
+if __name__ == '__main__':
+    app = wx.App()
+
+    main_win = mainWin(None)
+    main_win.SetTitle(u"tinyPySPEECH v0.1.0")
+    main_win.Show()
+
+    app.MainLoop()
